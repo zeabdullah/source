5e642dbaa7c7e668414f0f84e2a4195689c78854
Abdullah Z <dev@zabd.dev>
2025-08-25 00:38:44 +0300

    dep: installl & setup tailwindcss, prettier, & eslint

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('source-client');
+    protected readonly title = signal('source-client');
 }
