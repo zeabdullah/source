a8fdb2c41b2aeeab0271649cb501ac0c1d44ff02
Abdullah Z <dev@zabd.dev>
2025-08-28 00:34:32 +0300

    feat(frontend): configure HTTP client interceptor with base URL and required headers

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,9 +4,20 @@ import {
     provideZonelessChangeDetection,
 } from '@angular/core'
 import { provideRouter } from '@angular/router'
+import { provideHttpClient, withInterceptors, HttpInterceptorFn } from '@angular/common/http'
 
 import { routes } from './app.routes'
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser'
+
+const baseInterceptor: HttpInterceptorFn = (req, next) => {
+    const modifiedReq = req.clone({
+        url: 'http://localhost:8000/api' + req.urlWithParams,
+        setHeaders: {
+            Accept: 'application/json',
+        },
+    })
+    return next(modifiedReq)
+}
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -14,5 +25,6 @@ export const appConfig: ApplicationConfig = {
         provideZonelessChangeDetection(),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
+        provideHttpClient(withInterceptors([baseInterceptor])),
     ],
 }
