acd78f1df25b00eba73808d12b784544500bd6dd
Abdullah Z <dev@zabd.dev>
2025-09-03 22:13:31 +0300

    feat(plugin): implement listing frames' IDs

--- plugin/code.ts
+++ plugin/code.ts
@@ -9,29 +9,29 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__)
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = (msg: { type: string; count: number }) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-        // This plugin creates rectangles on the screen.
-        const numberOfRectangles = msg.count
+figma.on('selectionchange', () => {
+    const selectedFrames = figma.currentPage.selection.filter(node => node.type === 'FRAME')
+    figma.ui.postMessage({ type: 'frame-select', payload: selectedFrames })
+})
 
-        const nodes: SceneNode[] = []
-        for (let i = 0; i < numberOfRectangles; i++) {
-            const rect = figma.createRectangle()
-            rect.x = i * 150
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }]
-            figma.currentPage.appendChild(rect)
-            nodes.push(rect)
-        }
-        figma.currentPage.selection = nodes
-        figma.viewport.scrollAndZoomIntoView(nodes)
-    }
+// figma.ui.onmessage = (msg: { type: string; count: number }) => {
+//     if (msg.type === 'create-shapes') {
+//         // This plugin creates rectangles on the screen.
+//         const numberOfRectangles = msg.count
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin()
-}
+//         const nodes: SceneNode[] = []
+//         for (let i = 0; i < numberOfRectangles; i++) {
+//             const rect = figma.createRectangle()
+//             rect.x = i * 150
+//             rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }]
+//             figma.currentPage.appendChild(rect)
+//             nodes.push(rect)
+//         }
+//         figma.currentPage.selection = nodes
+//         figma.viewport.scrollAndZoomIntoView(nodes)
+//     }
+
+//     // Make sure to close the plugin when you're done. Otherwise the plugin will
+//     // keep running, which shows the cancel button at the bottom of the screen.
+//     figma.closePlugin()
+// }
