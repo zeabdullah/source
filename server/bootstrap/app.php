71cbad5953c92326f1fcb03863d5d27897c9f012
Abdullah Z <dev@zabd.dev>
2025-09-05 21:19:22 +0300

    feat(backend): create `EnsureUserIsProjectOwner` middleware
    Aliased with `is_owner`
    
    - delete Unit/ExampleTest

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureUserIsProjectOwner;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->alias([
+            'is_owner' => EnsureUserIsProjectOwner::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
