38ed67a0fa2e0028ababf13293002e1611c23313
Abdullah Z <dev@zabd.dev>
2025-09-30 12:50:04 +0300

    test(frontend): remove non-installed package from karma.conf.js

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -4,13 +4,12 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        frameworks: ['jasmine'],
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             jasmine: {
@@ -38,6 +37,7 @@ module.exports = function (config) {
                     '--no-sandbox',
                     '--disable-web-security',
                     '--disable-gpu',
+                    '--headless',
                     '--remote-debugging-port=9222',
                 ],
             },
